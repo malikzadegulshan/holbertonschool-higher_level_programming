196814f535c316f074dd3976245355fabbd3cfb4
Gulshan Malikzade <malikzade.gulshan@gmail.com>
2026-02-07 22:13:29 +0400

    updated file number 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 str = str.split()
-str = str[5:7] + [str[11]] + [str[0]]
-str = chr(32).join(str)
+str = chr(32).join(str[5:7] + [str[11]] + [str[0]])
 print(str)
