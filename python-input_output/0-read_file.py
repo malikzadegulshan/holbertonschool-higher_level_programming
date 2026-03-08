aa27c835df1241d7c32045c544d4b0d4fd483b0f
Gulshan Malikzade <malikzade.gulshan@gmail.com>
2026-03-08 17:54:25 +0400

    Adedd the module

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module for reading and printing a UTF-8 text file to stdout."""
+
+
 def read_file(filename=""):
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
