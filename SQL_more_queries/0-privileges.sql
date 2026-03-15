1d0c2bbfb85592bbbcab25cc169a94ec78004f53
Gulshan Malikzade <malikzade.gulshan@gmail.com>
2026-03-15 19:40:47 +0400

    Added more SQL queries folder

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Show all privileges granted to user_0d_1 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show all privileges granted to user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
