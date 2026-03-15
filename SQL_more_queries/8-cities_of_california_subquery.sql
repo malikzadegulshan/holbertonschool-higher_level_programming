26ad353df56b58d40bebcc8d87eac9fd44293dce
Gulshan Malikzade <malikzade.gulshan@gmail.com>
2026-03-15 19:57:17 +0400

    Added a script that lists all cities in California

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- List all cities in California using a subquery (no JOIN allowed)
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id          
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY id ASC;       
